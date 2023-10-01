1a8181968b407260c15fb43a63b0bcf33172df87
xenxorowdy <riyam.jain.3@hotmail.com>
2023-10-01 18:20:49 +0530

    skeleton

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -17,13 +17,13 @@ export default function Login() {
 
 
       <div className={"flex gap-14 items-center justify-center "}>
-      <p className="caption flex text-[#3f4550] font-normal text-3xl  align-baseline  items-center w-fit">Hello {session?.user?.name}</p> 
+      <p className=" flex text-[#3f4550] font-normal gap-10 text-3xl  align-baseline  items-center w-[240px]">Hello {session?.user?.name}</p> 
       <button  className="inline-flex items-center px-4 py-2 bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium rounded-md" onClick={async()=>{await signOut(); router.push("/login")}}> Sign Out</button>
       </div>
       <div className='bodycard'>
   
   <a href='/upload' >
-  <p className="caption flex text-[#3f4550] font-normal align-baseline  items-center  w-fit text-xl"> <FaCloudUploadAlt className="h-4"/> upload <FaExternalLinkAlt className='h-4 '/> </p>
+  <p className=" flex text-[#3f4550] font-normal gap-10 align-baseline  items-center  w-[240px] text-xl"> <FaCloudUploadAlt className="h-4"/> upload <FaExternalLinkAlt className='h-4 '/> </p>
 <figure>
 <Image  src={'/wp7388677-forklift-wallpapers.jpg'} className=' aspect-[3/2] w-80 h-40' width={300} height={400} alt="Mountains" />
 
@@ -32,7 +32,7 @@ export default function Login() {
   </a>
   <a href='/deleteImage' >
 
-  <p className="caption flex text-[#3f4550] font-normal align-baseline  items-center   text-xl w-fit"><FaTrashAlt className="h-4"/>  delete <FaExternalLinkAlt className='h-4'/> </p>
+  <p className=" flex text-[#3f4550] font-normal gap-10 align-baseline  items-center   text-xl w-[240px]"><FaTrashAlt className="h-4"/>  delete <FaExternalLinkAlt className='h-4'/> </p>
 <figure>
 
   <Image  src={'/forklift-for-rent.jpg'} className=' aspect-[3/2] w-80 h-40' width={300} height={400} alt="Mountains" />
