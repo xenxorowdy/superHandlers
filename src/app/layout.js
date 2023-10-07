9496c3ea199bf5145cef3b409e18057b17810832
xenxorowdy <riyam.jain.3@hotmail.com>
2023-10-07 12:56:58 +0530

    google analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Navbar from './component/Navbar'
 import SessionProvider from "./component/SessionProvider.jsx"
 import Footter from './component/footter'
 import { getServerSession } from 'next-auth'
+import Script from 'next/script'
 // require("../mongo/connect")
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,10 +20,25 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en">
 <head>
+  
 <link rel="icon" href="/superlogo.webp" />
 
   </head>      
+  <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
 
+      <Script id="google-analytics-script" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+          });
+    `}
+      </Script>
       <body className={inter.className}>
       <Navbar/>
       <SessionProvider session={session}>
