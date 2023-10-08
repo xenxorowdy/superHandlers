8907c918f592bc810e48bd81a3f3bffc3ddcbdbe
xenxorowdy <riyam.jain.3@hotmail.com>
2023-10-08 17:08:27 +0530

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,8 +25,13 @@ export default async function RootLayout({ children }) {
   
   
     <head>
-    <link rel="icon" href="/logo.webp" />
-    <link rel="apple-touch-icon" href="/logo.webp"/>
+
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+    <link rel="manifest" href="/site.webmanifest" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+    <meta name="msapplication-TileImage" content="/android-chrome-512x512.png"/>
 
 
     </head>
