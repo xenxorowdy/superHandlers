41bb3324f6c4d786e09f8ad645ce642c6e692113
Riyam Jain <59787534+xenxorowdy@users.noreply.github.com>
2026-03-30 02:57:20 +0530

    Update footter.jsx

--- src/app/component/footter.jsx
+++ src/app/component/footter.jsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image"
 import Link from "next/link"
 import { FaFacebook, FaTwitter, FaLinkedinIn, FaInstagram, FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
